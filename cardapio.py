ccb8deb8a6affe864c4ffef1294343a490ab7a62
olimpiodev.tec <olimpiodev.tec@gmail.com>
2025-11-28 22:59:22 +0000

    start project

---
+++ cardapio.py
@@ -0,0 +1,16 @@
+def get_cardapio():
+    cardapio = [
+        {
+            'id': 1,
+            'titulo': 'Bolo de Chocolate',
+            'descricao': 'Delicioso bolo de chocolate',
+            'foto': ''
+        },
+        {
+            'id': 2,
+            'titulo': 'Bolo de morango',
+            'descricao': 'Delicioso bolo de morango',
+            'foto': ''
+        }
+    ]
+    return cardapio
