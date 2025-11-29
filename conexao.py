2d8c9679e83ede1f9ee45a80098387ccad191572
olimpiodev.tec <olimpiodev.tec@gmail.com>
2025-11-29 01:01:15 +0000

    add .env

--- conexao.py
+++ conexao.py
@@ -1,11 +1,15 @@
 import psycopg2
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 def get_conexao():
     conn = psycopg2.connect(
-        dbname='postgres',
-        user='postgres.royfkjapbmqrdoprlydy',
-        password='CapivariIfsp',
-        host='aws-1-us-east-2.pooler.supabase.com',
-        port=6543
+        dbname=os.getenv('DB_DATABASE'),
+        user=os.getenv('DB_USER'),
+        password=os.getenv('DB_PASSWORD'),
+        host=os.getenv('DB_HOST'),
+        port=os.getenv('DB_PORT')
     )
     return conn
